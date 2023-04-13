d5b40aefd029178ffde2c64507bede1b4d168772
Ethan Water <angelshatepop@gmail.com>
2023-04-12 23:45:49 -0400

    revert implemented

--- organize.go
+++ organize.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
 	"io/ioutil"
@@ -11,9 +12,9 @@ import (
 	"github.com/schollz/progressbar/v3"
 )
 
-const TEXTS string = "/Users/xxx/Documents/texts/"
-const IMAGES string = "/Users/xxx/Documents/images/"
-const MISC string = "misc/"
+const ORIGIN string = "/Users/ethanwater/Downloads/"
+const TEXTS string = "/Users/ethanwater/Documents/texts/"
+const IMAGES string = "/Users/ethanwater/Documents/images/"
 
 func IsImage(file string) bool {
 	image := false
@@ -45,50 +46,51 @@ func IsText(file string) bool {
 }
 
 func Type(file string) string {
-	filetype := "null"
+	fileType := "null"
 	if IsImage(file) {
-		filetype = "img"
+		fileType = "img"
 	} else if IsText(file) {
-		filetype = "txt"
+		fileType = "txt"
 	} else {
-		filetype = "misc"
+		fileType = "misc"
 	}
 
-	return filetype
+	return fileType
 }
 
 func UnsafeOrganize(path string) {
 	if path == "" {
-		current_directory, err := os.Getwd()
+		currentDirectory, err := os.Getwd()
 		if err != nil {
 			log.Fatal(err)
 		}
-		path = current_directory
-		fmt.Printf("organizing: %s \n", path)
+		path = currentDirectory
 	}
+	fmt.Printf("organizing: %s \n", path)
 
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	bar := progressbar.Default(int64(len(files)))
+	bar := progressbar.DefaultBytes(
+		int64(len(files)),
+		"organizing...",
+	)
 
 	for _, file := range files {
-		old_path := path + file.Name()
+		oldPath := path + file.Name()
 
 		switch Type(file.Name()) {
 		case "img":
-			new_path := IMAGES + file.Name()
-			os.Rename(old_path, new_path)
+			newPath := IMAGES + file.Name()
+			os.Rename(oldPath, newPath)
 			bar.Add(1)
 		case "txt":
-			new_path := TEXTS + file.Name()
-			os.Rename(old_path, new_path)
+			newPath := TEXTS + file.Name()
+			os.Rename(oldPath, newPath)
 			bar.Add(1)
 		default:
-			new_path := MISC + file.Name()
-			os.Rename(old_path, new_path)
 			bar.Add(1)
 		}
 	}
@@ -106,6 +108,7 @@ func CheckLogStatus() bool { //can possibly be revoked
 }
 
 func SafeOrganize(path string) {
+	Clear()
 	//format path for files within for custom input directory
 	//weird bug, jump in len(files) ?
 	//if !strings.HasSuffix(path, "/") {
@@ -113,8 +116,8 @@ func SafeOrganize(path string) {
 	//}
 
 	//check if log exists, if not create (kind of useless tho ngl)
-	log_status := CheckLogStatus()
-	if log_status == false {
+	logStatus := CheckLogStatus()
+	if logStatus == false {
 		_, err := os.Create("backup.log")
 		if err != nil {
 			log.Fatal(err)
@@ -123,23 +126,26 @@ func SafeOrganize(path string) {
 
 	//default path declaration
 	if path == "" {
-		current_directory, err := os.Getwd()
+		currentDirectory, err := os.Getwd()
 		if err != nil {
 			log.Fatal(err)
 		}
-		path = current_directory + "/" //proper formatting of original path
-		fmt.Printf("organizing: %s \n", path)
+		path = currentDirectory + "/" //proper formatting of original path
 	}
+	fmt.Printf("organizing: %s \n", path)
 
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	bar := progressbar.Default(int64(len(files)))
+	bar := progressbar.DefaultBytes(
+		int64(len(files)),
+		"organizing...",
+	)
 
 	for _, file := range files {
-		old_path := path + file.Name()
+		oldPath := path + file.Name()
 		backup, err := os.OpenFile("backup.log",
 			os.O_APPEND|os.O_CREATE|os.O_WRONLY,
 			0644)
@@ -147,29 +153,76 @@ func SafeOrganize(path string) {
 			log.Fatal(err)
 		}
 
-		if _, err := backup.Write([]byte(old_path + "\n")); err != nil {
+		if _, err := backup.Write([]byte(file.Name() + "\n")); err != nil {
 			log.Fatal(err)
 		}
 
 		switch Type(file.Name()) {
 		case "img":
-			new_path := IMAGES + file.Name()
-			os.Rename(old_path, new_path)
+			newPath := IMAGES + file.Name()
+			os.Rename(oldPath, newPath)
 			bar.Add(1)
 		case "txt":
-			new_path := TEXTS + file.Name()
-			os.Rename(old_path, new_path)
+			newPath := TEXTS + file.Name()
+			os.Rename(oldPath, newPath)
 			bar.Add(1)
 		default:
-			new_path := MISC + file.Name()
-			os.Rename(old_path, new_path)
 			bar.Add(1)
 		}
 	}
 }
 
-func Revert() {}
+func Revert() {
+	readLog, err := os.Open("backup.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logScanner := bufio.NewScanner(readLog)
+	logScanner.Split(bufio.ScanLines)
+	var logLines []string
+
+	for logScanner.Scan() {
+		logLines = append(logLines, logScanner.Text())
+	}
+	readLog.Close()
+
+	if len(logLines) == 0 {
+		log.Fatal("nothing to revert")
+	}
+	fmt.Println("reverting changes")
+
+	bar := progressbar.DefaultBytes(
+		int64(len(logLines)),
+		"reverting...",
+	)
+
+	for _, line := range logLines {
+		originalPath := ORIGIN + line
+		switch Type(line) {
+		case "img":
+			newPath := IMAGES + line
+			os.Rename(newPath, originalPath)
+			bar.Add(1)
+		case "txt":
+			newPath := TEXTS + line
+			os.Rename(newPath, originalPath)
+			bar.Add(1)
+		default:
+			bar.Add(1)
+		}
+
+	}
+
+	Clear()
+}
+
+func Clear() {
+	if err := os.Truncate("backup.log", 0); err != nil {
+		log.Printf("failed to clear backup %s", err)
+	}
+}
 
 func main() {
-	SafeOrganize("/Users/xxx/Downloads/")
+	Revert()
 }
