75c7d83dbb876ebd0347d5c5c4290e6121804d50
Ethan Water <angelshatepop@gmail.com>
2023-04-13 01:19:45 -0400

    test

--- organize_test.go
+++ organize_test.go
@@ -4,10 +4,6 @@ import "testing"
 
 func TestLogStatus(t *testing.T) {
 	if CheckLogStatus() == false {
-<<<<<<< Updated upstream
-		t.Errorf("can't find log")
-=======
 		t.Errorf("backup.log not detected")
->>>>>>> Stashed changes
 	}
 }
