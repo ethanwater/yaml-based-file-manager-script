fbbd04176facc0bbe513e2ff823373993e91abdd
Ethan Water <angelshatepop@gmail.com>
2023-04-17 19:51:06 -0400

    Scan + Test

--- organize.go
+++ organize.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/schollz/progressbar/v3"
@@ -16,11 +17,24 @@ var ORIGIN string
 var TEXTS string
 var IMAGES string
 var CONFIGS []Config
+var CONFIG_FILE string
 
 type Config struct {
 	Name string
 	Path string
 	Ext  []string
+}
+
+func OpenConfig() {
+	cmdOpenConfig := exec.Command("open", CONFIG_FILE)
+	_, err := cmdOpenConfig.Output()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func SetConfig(config string) {
+	CONFIG_FILE = config
 }
 
 func Clear() {
@@ -51,31 +65,32 @@ func Configurations() {
 }
 
 func CreateConfig(config map[string]interface{}) Config {
+	//establishes a config form the YAML
 	var (
 		name string
 		path string
 		ext  string
 	)
 	//name
-	if n, ok := config["name"].(string); ok {
+	if n, err := config["name"].(string); err {
 		name = n
 	} else {
-		fmt.Println(ok)
+		fmt.Println(err)
 	}
 	//path
-	if p, ok := config["path"].(string); ok {
+	if p, err := config["path"].(string); err {
 		path = p
 		if name == "origin" {
 			ORIGIN = path
 		}
 	} else {
-		fmt.Println(ok)
+		fmt.Println(err)
 	}
 	//extensions
-	if e, ok := config["ext"].(string); ok {
+	if e, err := config["ext"].(string); err {
 		ext = e
 	} else {
-		fmt.Println(ok)
+		fmt.Println(err)
 	}
 
 	extensions := strings.Split(ext, " ")
@@ -88,6 +103,7 @@ func CreateConfig(config map[string]interface{}) Config {
 }
 
 func UnsafeOrganize() {
+	//Organizes the files in an unsafe manner, void of logging
 	Configurations()
 	path := ORIGIN
 
@@ -121,6 +137,8 @@ func UnsafeOrganize() {
 }
 
 func SafeOrganize() {
+	//Uses a logging system to log every file move executed. Creates/writes to a
+	//backup file, which is necessary to call Revert
 	Clear()
 	Configurations()
 	path := ORIGIN
@@ -165,6 +183,8 @@ func SafeOrganize() {
 }
 
 func Revert() {
+	//Reverts the previous SafeOrganization actions, if the backup is empty,
+	//this will do nothing
 	Configurations()
 
 	readLog, err := os.Open("backup.log")
@@ -206,6 +226,107 @@ func Revert() {
 	}
 }
 
+func Test() {
+	//Test is used to make sure that the Safe and Unsafe Organization functions
+	//run smoothly without any error. No files will be renamed/moved during this
+	//process.
+	Configurations()
+	path := ORIGIN
+
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(files) == 0 {
+		log.Fatal("nothing to organize")
+	}
+
+	fmt.Printf("organizing: %s \n", path)
+	bar := progressbar.DefaultBytes(
+		int64(len(files)),
+		"testing organize...",
+	)
+
+	for _, file := range files {
+		oldPath := path + file.Name()
+		for _, config := range CONFIGS {
+			for _, extension := range config.Ext {
+				if strings.Contains(oldPath, extension) {
+					break
+				}
+			}
+		}
+		bar.Add(1)
+	}
+	fmt.Println("Test Complete")
+}
+
+func Scan() {
+	//Scans the given directory and returns information about the directory and
+	//the files within
+	Configurations()
+	path := ORIGIN
+
+	hiddenCount := 0
+	directoryCount := 0
+	matchConfig := 0
+
+	directory, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	directoryStat, err := directory.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	files, err := ioutil.ReadDir(path)
+	fileCount := len(files)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(files) == 0 {
+		log.Fatal("nothing to organize")
+	}
+
+	fmt.Printf("organizing: %s \n", path)
+	bar := progressbar.DefaultBytes(
+		int64(fileCount),
+		"scanning...",
+	)
+
+	for _, file := range files {
+		if strings.HasPrefix(file.Name(), ".") {
+			hiddenCount += 1
+			bar.Add(1)
+			continue
+		} else if file.IsDir() {
+			directoryCount += 1
+			bar.Add(1)
+			continue
+		}
+
+		oldPath := path + file.Name()
+		for _, config := range CONFIGS {
+			for _, extension := range config.Ext {
+				if strings.Contains(oldPath, extension) {
+					matchConfig += 1
+					break
+				}
+			}
+		}
+		bar.Add(1)
+	}
+
+	fmt.Println("\nRESULTS")
+	fmt.Printf("directory stat: %+v\n", directoryStat)
+	fmt.Printf("total files: %d\n", fileCount)
+	fmt.Printf("hidden files: %d\n", hiddenCount)
+	fmt.Printf("cirectory count: %d\n", directoryCount)
+	fmt.Printf("matching configurations: %d\n", matchConfig)
+	fmt.Printf("non-matching configurations: %d\n", fileCount-matchConfig)
+}
+
 func main() {
-	SafeOrganize()
+	Scan()
 }
